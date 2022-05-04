13560db61f3a0cac3752b0e37efdd1a83ddbee65
Tony Tran <tonytranme0@hotmail.com>
2022-05-04 19:03:42 +1000

    finished exercise 03_reverseString, will need to complete all the tests for 02 exercise before moving on.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+let stringLength = string.length-1;
+
+let stringArray = [];
+
+while(stringLength >= 0){
+    stringArray.push(string[stringLength]);
+    stringLength-=1;
+
+}
+
+let returnString = stringArray.join("");
+
+
+return returnString;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;

2ede419c30b200ee165c31a4d101934ea0cefaa5
Tony Tran <tonytranme0@hotmail.com>
2022-05-18 20:35:27 +1000

    finish sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let progressNumber = 0;
+  let endNumber = 0;
+  let sum = 0;
 
+  if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    if (a < b) {
+      progressNumber = a;
+      endNumber = b;
+    } else {
+      progressNumber = b;
+      endNumber = a;
+    }
+
+    while (progressNumber <= endNumber) {
+      sum += progressNumber;
+      progressNumber++;
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line

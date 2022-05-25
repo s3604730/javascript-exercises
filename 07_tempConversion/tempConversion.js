3f8dd28d42af3fbb0c6770a84d9707b1a0310064
Tony Tran <tonytranme0@hotmail.com>
2022-05-25 21:34:54 +1000

    finished temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+ celsiusValue = (value-32) *(5/9);
+ roundedValue = Math.round(celsiusValue*10)/10;
+ return roundedValue;
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+   farenheitValue = (value*(9/5)+32);
+   roundedValue = Math.round(farenheitValue*10)/10;
+  return roundedValue;
 };
 
 // Do not edit below this line

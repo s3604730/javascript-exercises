5df0f9d027bef6f13ab8d76ad577c4b01c21385e
Tony Tran <tonytranme0@hotmail.com>
2022-06-07 22:17:09 +1000

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let b=0;
+	for(i=0; i<a.length;i++){
+    b = b + a[i];
+  }
+  return b;
 };
 
-const multiply = function() {
-
+//continue this part
+const multiply = function(a) {
+  b = 1;
+  for(i=0; i<a.length; i++){
+  b = b * a[i];
+  } 
+  return b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let c = Math.pow(a,b);
+  return c;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let x = 1;
+  if(a== 0 || a == 1){
+    return x;
+
+  }else{
+    for(var i = a; i>=1; i--){
+     
+      x = x * i;
+    }
+    return x;
+
+  }
 };
 
 // Do not edit below this line

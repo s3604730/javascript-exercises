9653c4d5a8784dedbe17dbd6d635385c0703464a
Tony Tran <tonytranme0@hotmail.com>
2022-06-10 14:17:59 +1000

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (a) {
+let b = a.toLowerCase();
+let newString="";
+let c = b.replace(/[.,/#!$%^&*;:{}=-_`~()]/g, "");
+let d = c.replace(/\s/g, '');
+for(var i =d.length-1; i >=0; i--){
+    newString+=d[i];
+
+
+}
+console.log(d);
+console.log(newString);
+
+if (d == newString){
+    return true;
+}else{
+    return false;
+}
 
 };
-
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;

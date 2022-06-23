e4f06f8230975c3e4b5ef1d711a392b63874e6fd
Tony Tran <tonytranme0@hotmail.com>
2022-06-23 20:18:01 +1000

    completed a majority of the tasks, requires accepting string and returning string as well as not accepting negatives

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    if(n <= 1){
+        return n;
+    
+    }
+    let a = fibonacci(n-1)+ fibonacci(n-2);
+    let b = Number(a);
+    console.log(b);
+    return b;
 
 };
+
+fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;

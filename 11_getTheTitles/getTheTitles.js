8720d979ec053099ed572c538c4020b1613b923b
Tony Tran <tonytranme0@hotmail.com>
2022-07-04 23:56:45 +1000

    completed getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object1) {
+    let result = object1.map(a => a.title);
+ 
+    return result
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;

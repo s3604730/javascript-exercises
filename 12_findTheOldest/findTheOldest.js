d29b8debb3af30e5ec020389867d5b2325dfdc95
Tony Tran <tonytranme0@hotmail.com>
2022-07-11 21:38:57 +1000

    finished find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(object) {
+    let oldestPerson = null;
+    let oldestAge = 0;
+    for(const element of object){
+
+        if (element.yearOfDeath == null){
+            const currentYear = new Date().getFullYear();
+            element.yearOfDeath = currentYear;
+              
+        }
+        if ((element.yearOfDeath - element.yearOfBirth) > oldestAge){
+            oldestPerson = element;
+            oldestAge = element.yearOfDeath - element.yearOfBirth;
+    }
+    }
+
+    return oldestPerson;
 
 };
 
